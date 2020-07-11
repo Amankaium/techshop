f7252b8ba15499f178d3fd16d675b95f00ee1786
Amankaium <kaium@list.ru>
2020-07-11 20:05:33 +0600

    added product edit

--- product/urls.py
+++ product/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("all/", products, name="products"),
     path("<int:id>/", product, name="product"),
     path("create/", product_create, name="product-create"),
+    path("edit/<int:id>/", product_edit, name="product-edit"),
 ]
--- product/views.py
+++ product/views.py
@@ -30,3 +30,22 @@ def product_create(request):
         "product/form.html",
         context
     )
+
+
+def product_edit(request, id):
+    product = Product.objects.get(id=id)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("product", id=product.id)
+    
+    context = {}
+    context["form"] = ProductForm(instance=product)
+
+    return render(
+        request,
+        "product/form.html",
+        context
+    )
