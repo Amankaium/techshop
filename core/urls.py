79b6cb45eb69075424a8517a1f5f5347af9dff77
Amankaium <kaium@list.ru>
2020-07-14 21:04:20 +0600

    added login, logout and reg (not finished)

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, test
+from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
     path("test/", test, name="test"),
+    path("login/", login, name="login"),
+    path("logout/", logout, name="logout"),
+    path("registration/", registration, name="registration")
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,\
     HttpResponse, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from core.forms import RegistrationForm
 
 
 def test(request):
@@ -7,4 +10,39 @@ def test(request):
 
 
 def home(request):
+    print("------------")
+    print("Пользователь:")
+    print(request.user)
+    print("------------")
     return redirect("products")
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect(home)
+    context = {}
+    if "login" in request.POST:
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            return redirect(home)
+
+    context["form"] = AuthenticationForm()
+    return render(request, "core/login.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(home)
+
+
+def registration(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(home)
+    context = {}
+    context["form"] = RegistrationForm()
+    return render(request, "core/registration.html", context)
