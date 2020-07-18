0b94da240de1becd1adbaf23d20adde7c66920c2
Amankaium <kaium@list.ru>
2020-07-18 21:02:55 +0600

    added ProductAdmin

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from product.models import *
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    # fields = [field.name for field in Product._meta.get_fields()[1:]]
+    readonly_fields = ["user", "sales", "price", "avialable", "deleted"]
+    list_display = [field.name for field in Product._meta.get_fields()]
+    list_display_links = ("id", "name", "user")
+    list_editable = ("price",)
+    search_fields = ["name", "description", "user__username"]
+    list_filter = ["category", "name"]
+    list_per_page = 10
 
-admin.site.register(Product)
+
 admin.site.register(Category)
