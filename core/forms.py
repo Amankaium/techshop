9e03a66eecf927866e70f968d960c01b0d330e78
Amankaium <kaium@list.ru>
2020-07-21 20:40:57 +0600

    registration v1.1

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import User
 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+    
     class Meta:
         model = User
-        fields = ["username", "password", "first_name", "last_name"]
+        fields = ["username", "email", "first_name", "last_name"]
+    
+    def save(self):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.save()
+        return user
