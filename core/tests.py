0dead72ed97df1c7e51a8a646c35ada74e10eb70
Amankaium <kaium@list.ru>
2020-08-15 21:17:45 +0600

    added testing

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from selenium import webdriver
+from time import sleep
 
-# Create your tests here.
+
+class HomepageTestCase(TestCase):
+    def setUp(self):
+        c = Client()
+        self.response = c.get("/")
+
+    # def tearDown(self):
+    def test_redirect_from_homepage_302_success(self):
+        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(self.response.url, reverse("products"))
+
+    def test_open_homepage_200_fail(self):
+        self.assertNotEqual(self.response.status_code, 200)
+
+
+class AuthTestCase(TestCase):
+    def test_load_auth_page_success(self):
+        driver = webdriver.Chrome()
+        driver.get("localhost:8000/")
+
+        # driver.find_element_by_xpath("//a[@href='/login/']").click()
+        driver.find_element_by_xpath("//a[contains(@href, 'login')]").click()
+        
+        # assert "form" in driver.page_source
+        self.assertIn("form", driver.page_source)
+
+        # sleep(3)
--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Product
 
-# Create your tests here.
+
+class ProductsTestCase(TestCase):
+    def setUp(self):
+        products_url = reverse("products")
+        c = Client()
+        self.response = c.get(products_url)
+        self.products = Product.objects.all()
+
+    def test_products_open_200_OK(self):
+        self.assertEqual(self.response.status_code, 200)
+        # print(self.response.content)
+        self.assertContains(self.response, "Все товары")
+        self.assertContains(self.response, "Все продавцы")
